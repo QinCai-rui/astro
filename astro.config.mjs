7713eb58723854e5acad53d467986390bd36e63f
QinCai-rui <raymontqin_rui@outlook.com>
2025-05-02 16:11:56 +1200

    update for cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     server: {
       allowedHosts: ['astro.qincai.xyz']
     }
-  }
+  },
+
+  adapter: cloudflare()
 });
